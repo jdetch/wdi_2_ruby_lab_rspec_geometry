1fe64cf8668058ec343cdd8db3799518ff6f4651
Jodi Detch <jodidetch@gmail.com>
2014-06-17 11:45:46 -0400

    Rectangles can be created with a length and width

---
+++ spec/rectangle_spec.rb
@@ -0,0 +1,15 @@
+# 1 describe class
+# 2 describe method
+# 3 describe individual, specific test case
+require_relative '../lib/rectangle'
+
+describe Rectangle do
+  describe '#initialize' do
+    it 'accepts a length and a width' do
+      rectangle = Rectangle.new(6, 4)
+
+      expect(rectangle.length).to eq 6
+      expect(rectangle.width).to eq 4
+    end
+  end
+end
