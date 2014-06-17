909bf9181d868da30613f7c20fef589e39fa77f6
Jodi Detch <jodidetch@gmail.com>
2014-06-17 14:35:32 -0400

    Used 'contains exactly' matcher

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   attr_reader :sides
 
   def initialize(side_a, side_b, side_c)
-    @sides = [side_a, side_b, side_c]
+    @sides = [side_a, side_b, side_c].sort
 
     unless @sides[0] + @sides[1] > @sides[2]
       raise ArgumentError, 'triangle is invalid'
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -5,7 +5,8 @@ describe Triangle do
     it 'has three sides' do
       triangle = Triangle.new(5, 4, 3)
 
-      expect(triangle.sides).to eq [5, 4, 3]
+      expect(triangle.sides).to contain_exactly(5, 4, 3)
+      # or: expect(triangle.sides).to match_array [5, 4, 3]
      end
    end
 
@@ -13,6 +14,7 @@ describe Triangle do
     it 'prevents invalid triangles from being created' do
       # Need it like this because the block will leave the code 'as is' and pass it to expect
       expect{ Triangle.new(3, 4, 8) }.to raise_error ArgumentError
+      expect{ Triangle.new(8, 2, 3) }.to raise_error ArgumentError
     end
   end
 
