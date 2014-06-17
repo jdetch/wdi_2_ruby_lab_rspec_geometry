38b9ea74c507db711d78376ac7879f83ac1bc059
Jodi Detch <jodidetch@gmail.com>
2014-06-17 12:03:03 -0400

    Area and Perimeter can be calculated for rectangles

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -4,4 +4,13 @@ class Rectangle
   def initialize(length, width)
     @length, @width = length, width
   end
+
+  def area
+    length * width
+  end
+
+  def perimeter
+    2 * (length + width)
+  end
+
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -12,4 +12,22 @@ describe Rectangle do
       expect(rectangle.width).to eq 4
     end
   end
+
+  # We are still in the describe Rectangle block, but outside of describe initialize block
+  describe '#area' do
+    it 'calculates the area' do
+      rectangle = Rectangle.new(10, 2)
+
+      expect(rectangle.area).to eq 20
+    end
+  end
+
+  describe '#perimeter' do
+    it 'calculates the perimeter' do
+      rectangle = Rectangle.new(7, 5)
+
+      expect(rectangle.perimeter).to eq 24
+    end
+  end
+
 end
